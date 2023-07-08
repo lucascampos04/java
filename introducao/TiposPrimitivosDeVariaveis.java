b24d55328d3c31146122b43f39d0031738079d28
lucascampos04 <camposdlucasoli@gmail.com>
2023-07-08 15:24:31 -0300

    Tipos primitivos

---
+++ introducao/TiposPrimitivosDeVariaveis.java
@@ -0,0 +1,11 @@
+package introducao;
+
+public class TiposPrimitivosDeVariaveis {
+    public static void main(String[] args){
+        System.out.println("Tipos Primitivos");
+        System.out.println("---------------------------------");
+        System.out.println("int, double, float, char, byte, short, long, boolean");
+
+
+    }
+}
