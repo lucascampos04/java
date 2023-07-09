e8d2d9294b698b05fb422dd211e23ff564e39d8f
lucascampos04 <camposdlucasoli@gmail.com>
2023-07-09 14:48:27 -0300

    Array parte 2

---
+++ introducao/Arrays_pt2.java
@@ -0,0 +1,18 @@
+package introducao;
+
+public class Arrays_pt2 {
+    public static void main(String[] args) {
+
+// byte, short, int, float e double = 0
+        // char '\u0000'
+        // boolena false
+        // String null
+        char[] idade = new char[5];
+        idade[0] = 10;
+        idade[1] = 20;
+        idade[2] = 30;
+        System.out.println(idade[0]);
+
+
+    }
+}
