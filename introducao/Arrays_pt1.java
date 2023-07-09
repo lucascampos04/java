a5734c9148c85ec738b81a7651875920518e3052
lucascampos04 <camposdlucasoli@gmail.com>
2023-07-09 14:48:19 -0300

    Array parte 1

---
+++ introducao/Arrays_pt1.java
@@ -0,0 +1,17 @@
+package introducao;
+
+public class Arrays_pt1 {
+    public static void main(String[] args) {
+        int[] idade = new int[3];
+        idade[0] = 1;
+        System.out.println(idade[0]);
+
+        double[] idade1 = new double[5];
+        idade1[0] = 10;
+        idade1[1] = 20;
+        idade1[2] = 30;
+        System.out.println(idade1[0]);
+
+        // Array = Objetos
+    }
+}
