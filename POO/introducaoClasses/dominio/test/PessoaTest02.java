d50eef94ffa8030fc69973cbde9be1affde2d130
lucascampos04 <camposdlucasoli@gmail.com>
2023-07-10 16:14:47 -0300

    Introdução a classes pt2

---
+++ POO/introducaoClasses/dominio/test/PessoaTest02.java
@@ -0,0 +1,11 @@
+package POO.introducaoClasses.dominio.test;
+
+import POO.introducaoClasses.dominio.Estudante;
+
+public class PessoaTest02 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+
+        System.out.println(estudante.nome);
+    }
+}
