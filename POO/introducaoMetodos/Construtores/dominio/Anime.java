b62498663b64183423c523dedfb943ef74f290b1
lucascampos04 <camposdlucasoli@gmail.com>
2023-07-14 15:15:14 -0300

    Construtores, sobrecarga e construtores parte 2

--- POO/introducaoMetodos/Construtores/dominio/Anime.java
+++ POO/introducaoMetodos/Construtores/dominio/Anime.java
@@ -4,8 +4,10 @@ import POO.introducaoMetodos.Construtores.test.AnimeTest02;
 
 public class Anime {
     public static void main(String[] args) {
-        AnimeTest02 anime = new AnimeTest02("Naruto", "Anime", 200, "Ação");
+        AnimeTest02 anime = new AnimeTest02("Naruto", "Anime", 200, "Ação", "Hollywood");
         AnimeTest02 ani = new AnimeTest02();
+        ani.imprmir();
+        System.out.println("");
         anime.imprmir();
     }
 }
