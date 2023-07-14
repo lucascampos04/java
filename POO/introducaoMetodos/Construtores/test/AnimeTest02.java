c8ff0df2afd4dbc4b54ccf453fd62c2ca33aa682
lucascampos04 <camposdlucasoli@gmail.com>
2023-07-14 15:15:14 -0300

    Construtores, sobrecarga e construtores parte 2

--- POO/introducaoMetodos/Construtores/test/AnimeTest02.java
+++ POO/introducaoMetodos/Construtores/test/AnimeTest02.java
@@ -7,16 +7,19 @@ public class AnimeTest02 {
     private String nome;
     private int episodios;
     private String genero;
+    private String estudio;
 
     public void imprmir(){
         System.out.println("Tipo: " + this.tipo);
         System.out.println("Quantidade de episodios do anime: " + this.episodios);
         System.out.println("Nome do anime: " + this.nome);
         System.out.println("Genero do anime: " + this.genero);
+        System.out.println("Estudio: " + this.estudio);
     }
 
     // Construtor
     public AnimeTest02(String nome, String tipo, int episodios, String genero){
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
@@ -24,11 +27,13 @@ public class AnimeTest02 {
     }
 
     // Sobrecarga de construtores
-    public AnimeTest02(){
-
+    public AnimeTest02(String nome, String tipo, int episodios, String genero, String estudio){
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
     }
-
-
+    public AnimeTest02(){
+        System.out.println("teste");
+    }
     public void setNome(String nome) {
         this.nome = nome;
     }
@@ -53,5 +58,12 @@ public class AnimeTest02 {
     }
     public void setGenero(String genero) {
         this.genero = genero;
+    }
+    public String getEstudio() {
+        return estudio;
+    }
+
+    public void setEstudio(String estudio) {
+        this.estudio = estudio;
     }
 }
