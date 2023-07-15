42184104f278e7917c50847f3c64264d09d24fce
lucascampos04 <camposdlucasoli@gmail.com>
2023-07-15 16:23:16 -0300

    Associação Unidirecional pt1

--- POO/Associacao/dominio/Jogador.java
+++ POO/Associacao/dominio/Jogador.java
@@ -1,34 +1,11 @@
 package POO.Associacao.dominio;
 
-<<<<<<< HEAD
-import POO.AssociacaoUnidirecional.dominio.Time;
-
-public class Jogador {
-    private String nome;
-    private Time time;
-    public void nextUp(){
-        System.out.println(this.nome);
-        if (time == null){
-            System.out.println(time.getNome());
-            return;
-        }
-        System.out.println(time.getNome());
-    }
-    public Time getTime() {
-        return time;
-    }
-    public void setTime(Time time) {
-        this.time = time;
-    }
-
-=======
 public class Jogador {
     private String nome;
 
     public void nextUp(){
         System.out.println(this.nome);
     }
->>>>>>> 2ab646b (Associação pt1)
     public Jogador(String nome){
         this.nome = nome;
     }
