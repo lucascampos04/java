2ab646bab048d9aba8bd63fa2444deaff73b319a
lucascampos04 <camposdlucasoli@gmail.com>
2023-07-15 15:47:02 -0300

    Associação pt1

---
+++ POO/Associacao/dominio/Jogador.java
@@ -0,0 +1,18 @@
+package POO.Associacao.dominio;
+
+public class Jogador {
+    private String nome;
+
+    public void nextUp(){
+        System.out.println(this.nome);
+    }
+    public Jogador(String nome){
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
