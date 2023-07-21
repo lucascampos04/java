963e1227ff2cb5be16cf070ddcd68e8f6dfdc9c7
lucascampos04 <camposdlucasoli@gmail.com>
2023-07-21 12:25:05 -0300

    Heranças

---
+++ POO/Heranca/test/HerancaTest01.java
@@ -0,0 +1,26 @@
+package POO.Heranca.test;
+
+import POO.Heranca.dominio.Endereco;
+import POO.Heranca.dominio.Funcionario;
+import POO.Heranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 1");
+        endereco.setCep("12345-6789");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Lucas");
+        pessoa.setCpf("1111111");
+        pessoa.setEndereco(endereco);
+        pessoa.nextUp();
+        System.out.println("------------------------");
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Lucas");
+        funcionario.setSalario(2000.00);
+        funcionario.setCpf("2323123241");
+        funcionario.setEndereco(endereco);
+
+        funcionario.nextUp();
+    }
+}
