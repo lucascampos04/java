a1551535f411c5967af615ebb7d80876ac5a5676
lucascampos04 <camposdlucasoli@gmail.com>
2023-07-21 20:26:15 -0300

    uso do final em objetos

--- POO/ModificadorFinal/dominio/Carro.java
+++ POO/ModificadorFinal/dominio/Carro.java
@@ -2,11 +2,10 @@ package POO.ModificadorFinal.dominio;
 
 public class Carro {
     private String carro;
+
+    public final Comprador COMPRADOR = new Comprador();
     public static double VELOCIDADE_LIMITE = 250; // constantes em java são definidas em UPPERCASE
 
-    public Carro(){
-        VELOCIDADE_LIMITE = 250;
-    }
     public String getCarro() {
         return carro;
     }
--- POO/ModificadorFinal/test/CarroTest01.java
+++ POO/ModificadorFinal/test/CarroTest01.java
@@ -1,10 +1,14 @@
 package POO.ModificadorFinal.test;
 
 import POO.ModificadorFinal.dominio.Carro;
+import POO.ModificadorFinal.dominio.Comprador;
 
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
-        Carro.VELOCIDADE_LIMITE = 20;
+        System.out.println(Carro.VELOCIDADE_LIMITE = 250);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Corsa");
+        System.out.println(carro.COMPRADOR);
     }
 }
