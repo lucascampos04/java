7d5f1514d7c803635c3792421c50d0b7f9d84ff7
lucascampos04 <camposdlucasoli@gmail.com>
2023-07-24 17:31:21 -0300

    Classe Enumeração pt3 uso de uma classe abstrata

--- POO/Enumeracao/test/ClienteTest01.java
+++ POO/Enumeracao/test/ClienteTest01.java
@@ -2,12 +2,14 @@ package POO.Enumeracao.test;
 
 import POO.Enumeracao.dominio.Cliente;
 import POO.Enumeracao.dominio.TipoCliente;
+import POO.Enumeracao.dominio.TipoPagamento;
 
-public class ClineteTest01 {
+public class ClienteTest01 {
     public static void main(String[] args) {
         Cliente cliente = new Cliente("Lucas", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
         Cliente cliente1 = new Cliente("Lucas", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
 
+        System.out.println("Desconto: " + TipoPagamento.DEBITO.calcularDesconto(100) + "%");
         System.out.println(cliente);
         System.out.println(cliente1);
     }
