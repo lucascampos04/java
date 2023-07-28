98f82f63b7443cbac5791e4a1ca807ca60d90e27
lucascampos04 <camposdlucasoli@gmail.com>
2023-07-28 17:37:52 -0300

    Erro  StackOverFlow

--- POO/Exception/error/test/StackOverflowTest01.java
+++ POO/Exception/error/test/StackOverflowTest01.java
@@ -6,7 +6,8 @@ public class StackOverflowTest01 {
     public static void main(String[] args) {
         recursividade();
     }
-    public static void recursividade(){
+
+    public static void recursividade() {
         recursividade();
     }
 }
