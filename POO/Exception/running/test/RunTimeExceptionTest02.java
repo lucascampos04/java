2eec50a9cc7cc5b42bddddd9a3e5fd229801b1f5
lucascampos04 <camposdlucasoli@gmail.com>
2023-08-01 14:43:41 -0300

    Laçando exceção unchecked

---
+++ POO/Exception/running/test/RunTimeExceptionTest02.java
@@ -0,0 +1,20 @@
+package POO.Exception.running.test;
+
+public class RunTimeExceptionTest02 {
+    public static void main(String[] args) {
+        try {
+            divisao(1, 1);
+        } catch (RuntimeException e){
+            e.printStackTrace();
+        }
+        System.out.println("Codigo finalizado");
+    }
+    private static int divisao(int a, int b) throws IllegalArgumentException{
+        if(b == 0){
+            // Exception
+            throw new IllegalArgumentException("Argumento invalido");
+        }
+        return a/b;
+    }
+
+}
