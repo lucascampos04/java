d460a8781d5fd9719730b63ffb59f7c732a68bda
lucascampos04 <camposdlucasoli@gmail.com>
2023-08-19 19:16:40 -0300

    Classes Wrappers

---
+++ POO/ClassesUtilitarias/Test/WrapperTest01.java
@@ -0,0 +1,26 @@
+package POO.ClassesUtilitarias.Test;
+
+import java.awt.geom.Arc2D;
+
+public class WrapperTest01 {
+    // Wrappers são objetos que vão encapsular os tipos primitivos
+    // Tipos primitivos:
+    byte byteP = 1;
+    short shortP = 1;
+    int intP = 1;
+    long longP = 10L;
+    float floatP = 10F;
+    double doubleP = 1;
+    char charP = 'W';
+    boolean booleanP = true;
+
+    // Classes tipo Wrapper
+    Byte byteW = 1;
+    Short shortW = 1;
+    Integer intW = 1;
+    Long longW = 10L;
+    Float floatW = 10F;
+    Double doubleW = 10D;
+    Character charW = 'W';
+    Boolean booleanW = true;
+}
