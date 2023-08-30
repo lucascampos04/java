55503d37468c6977856819b0257f7c46793b5d86
lucascampos04 <camposdlucasoli@gmail.com>
2023-08-30 15:53:30 -0300

    Classe date

--- POO/ClassesUtilitarias/Date/Test/DateTest01.java
+++ POO/ClassesUtilitarias/Date/Test/DateTest01.java
@@ -6,8 +6,5 @@ public class DateTest01 {
     public static void main(String[] args) {
         Date date = new Date(1693420920337L); // Date trabalha com milesegundos = long 100000
         System.out.println(date);
-
-
-
     }
 }
